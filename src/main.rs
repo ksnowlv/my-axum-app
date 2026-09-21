a698cbba04b2a25a00a6bd3f2538cfd1951a220e
ksnowlv <air.lv@lbk.one>
2026-09-21 18:27:31 +0800

    添加axum 框架支持，helloworld示例

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on http://{}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
 }
